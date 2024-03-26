2780fe052fecd8e028314b24e8ba15d46f7b200b
dharmesh <dhiru7321r@gmail.com>
2024-03-26 19:37:39 +0530

    added dashboard

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const sendReq = async () => {
+    try {
+      const userData = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      };
+      const res = await axios
+        .post("http://localhost:5000/api/signup", userData)
+        .catch((e) => console.log(e));
+      const result = await res.data;
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await sendReq();
+    if (user) {
+      navigate("/login");
+    }
+  };
+  return (
+    <div className="mt-7">
+      <div className="w-1/3 m-auto ">
+        <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Name
+            <input
+              name="name"
+              type="text"
+              className="outline ml-10"
+              value={data.name}
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter your name"
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              name="email"
+              type="email"
+              className="outline ml-10"
+              value={data.email}
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter your email"
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              name="password"
+              type="password"
+              className="outline ml-10"
+              value={data.password}
+              onChange={(e) => handleChange(e)}
+              placeholder="Enter your password"
+            />
+          </label>
+          <button className="border-2" type="submit">
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+let firstRender = true;
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [data, setData] = useState({});
+
+  const refreshToken = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/refresh", {
+        withCredentials: true,
+      });
+      const result = await res.data;
+      setData(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendReq = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/user", {
+        withCredentials: true,
+      });
+      const result = await res.data;
+      setData(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (firstRender) {
+      sendReq();
+      firstRender = false;
+    }
+    let interval = setInterval(() => {
+      refreshToken();
+    }, 1000 * 29);
+    return () => clearInterval(interval);
+  }, []);
+  return <div>{data && <span>{data.name}'s</span>} Dashboard</div>;
 };
 
 export default Dashboard;
